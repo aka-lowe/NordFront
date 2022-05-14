c8a31909267244ed87746fd17948393e5abfcd92
aka-lowe <s215170@student.dtu.dk>
2022-05-14 04:35:58 +0200

    adasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Injectable()
+export class TriangleService {
+  constructor(private http:HttpClient) {
+
+  private static rest_url = "http://localhost:8081/?sideA='+this.sideA+'&sideB='+this.sideB'&sideC='+this.sideC+'";
+
+  async fetchType() {
+    try{
+      const data: any = await this.http.get(TriangleService.rest_url).toPromise();
+      return data;
+    } catch (error) {
+      console.error("Error occurred:" + error);
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [TriangleService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NordFront';
+  trType: any = {};
+
+  constructor(private triangleService: TriangleService) { }
+//       sideForm = new FormGroup({
+//         sideA: new FormControl(''),
+//         sideA: new FormControl(''),
+//         sideA: new FormControl('')
+//       });
+  ngOnInit() {
+    this.triangleService.fetchType().then(data => {
+      this.trType = data;
+    });
+  }
+
+
 }
+
+
+
+// , formBuilder: FormBuilder
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideEditorComponent } from './side-editor/side-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideEditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormGroup,
+    FormControl,
+    HttpClient,
+    Injectable,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
